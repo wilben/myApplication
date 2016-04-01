3a1c237ab96124db6d7910a96b52155441001f10
wilben <459664423@qq.com>
2016-04-01 10:28:10 +0800

    Initial commit

---
+++ app/src/main/java/com/wilben/test/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.wilben.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+}
+
